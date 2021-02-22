4195aa28c7d6f8157888c4c9efa45d651f55e917
d.klopov <strvnge1488>
2021-02-22 11:14:48 +0300

    add users to notify_all: Иванова

--- vlg_worker_bot.py
+++ vlg_worker_bot.py
@@ -66,10 +66,11 @@ def notify_all(message):
 
 
 def send_notify_all(notify_all_message):
-    bot.send_message(380895469, notify_all_message.text)
-    bot.send_message(676190873, notify_all_message.text)
-    bot.send_message(419881751, notify_all_message.text)
-    bot.send_message(790261504, notify_all_message.text)
+    bot.send_message(380895469, notify_all_message.text)  # Клопов
+    bot.send_message(676190873, notify_all_message.text)  # Спиридонов
+    bot.send_message(419881751, notify_all_message.text)  # Ефимов
+    bot.send_message(790261504, notify_all_message.text)  # Лисицкий
+    bot.send_message(790261504, notify_all_message.text)  # Иванова
 
 
 bot.polling(none_stop=True, interval=0)
